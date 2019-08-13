3d42431efb33ff2581951e2000318c9001d4a2d5
Son Nguyen <son.nguyen@be.xyz>
2019-08-13 15:53:24 +0700

    add new format for eslint

--- components/searchComponent/searchBlock.js
+++ components/searchComponent/searchBlock.js
@@ -1,117 +1,117 @@
-import React, { useState, useEffect } from "react";
-import Router from "next/router";
-import css from "styled-jsx/css";
-import { string } from "prop-types";
-import Select from "react-select";
-import { Row, Col } from "antd";
-import { find, get } from "lodash";
+import React, { useState, useEffect } from 'react'
+import Router from 'next/router'
+import css from 'styled-jsx/css'
+import { string } from 'prop-types'
+import Select from 'react-select'
+import { Row, Col } from 'antd'
+import { find, get } from 'lodash'
 
-import PopoverSlider from "./PopoverSlider";
-import { provinces, districts } from "../../constants/administrativeUnit";
-import { TYPE } from "../../constants";
+import PopoverSlider from './PopoverSlider'
+import { provinces, districts } from '../../constants/administrativeUnit'
+import { TYPE } from '../../constants'
 
-const maxPrice = 30000000000;
-const maxArea = 500;
+const maxPrice = 30000000000
+const maxArea = 500
 
 const priceMark = {
   0: {
     style: {
-      marginLeft: "35px",
-      color: "#222",
+      marginLeft: '35px',
+      color: '#222',
     },
-    label: "Giá tối thiểu",
+    label: 'Giá tối thiểu',
   },
   30000000000: {
     style: {
-      color: "#222",
-      left: "none",
-      right: "0",
-      transform: "none",
+      color: '#222',
+      left: 'none',
+      right: '0',
+      transform: 'none',
     },
-    label: "Giá tối đa",
+    label: 'Giá tối đa',
   },
-};
+}
 
 const areaMark = {
   0: {
     style: {
-      marginLeft: "53px",
-      color: "#222",
+      marginLeft: '53px',
+      color: '#222',
     },
-    label: "Diện tích tối thiểu",
+    label: 'Diện tích tối thiểu',
   },
   500: {
     style: {
-      color: "#222",
-      left: "none",
-      right: "0",
-      transform: "none",
+      color: '#222',
+      left: 'none',
+      right: '0',
+      transform: 'none',
     },
-    label: "Diện tích tối đa",
+    label: 'Diện tích tối đa',
   },
-};
+}
 
 const Search = props => {
-  const { type = "", location = "" } = props;
+  const { type = '', location = '' } = props
 
   // get default value from url
-  const defaultType = find(TYPE, item => item.value === type);
-  const defaultProvince = find(provinces, item => item.type === location);
-  const [province, setProvince] = useState(defaultProvince);
-  const [district, setDistrict] = useState();
-  const [houseType, setHouseType] = useState(defaultType);
-  const [districtsShow, setDistrictsShow] = useState([]);
+  const defaultType = find(TYPE, item => item.value === type)
+  const defaultProvince = find(provinces, item => item.type === location)
+  const [province, setProvince] = useState(defaultProvince)
+  const [district, setDistrict] = useState()
+  const [houseType, setHouseType] = useState(defaultType)
+  const [districtsShow, setDistrictsShow] = useState([])
   // for price
-  const [priceConfig, setPriceConfig] = useState([0, maxPrice]);
-  const [priceStep, setPriceStep] = useState(100000000);
+  const [priceConfig, setPriceConfig] = useState([0, maxPrice])
+  const [priceStep, setPriceStep] = useState(100000000)
   // for area
-  const [areaConfig, setAreaConfig] = useState([0, maxArea]);
-  const [areaStep, setAreaStep] = useState(5);
+  const [areaConfig, setAreaConfig] = useState([0, maxArea])
+  const [areaStep, setAreaStep] = useState(5)
 
   const chooseProvince = data => {
-    setProvince(data);
-    setDistrictsShow(districts[data.type]);
-  };
+    setProvince(data)
+    setDistrictsShow(districts[data.type])
+  }
 
   const chooseDistrict = data => {
-    setDistrict(data);
-  };
+    setDistrict(data)
+  }
 
   const chooseHouseType = data => {
-    setHouseType(data);
-  };
+    setHouseType(data)
+  }
 
   // for price
   const changePrice = value => {
-    if (value[0] > 7000000000) setPriceStep(500000000);
-    if (value[0] <= 7000000000) setPriceStep(100000000);
-    setPriceConfig(value);
-  };
+    if (value[0] > 7000000000) setPriceStep(500000000)
+    if (value[0] <= 7000000000) setPriceStep(100000000)
+    setPriceConfig(value)
+  }
 
   const fillterPrice = () => {
-    console.log("douma ahihi");
-  };
+    console.log('douma ahihi')
+  }
 
   // for area
   const changeArea = value => {
-    if (value[0] > 200) setAreaStep(50);
-    if (value[0] <= 200) setAreaStep(5);
-    setAreaConfig(value);
-  };
+    if (value[0] > 200) setAreaStep(50)
+    if (value[0] <= 200) setAreaStep(5)
+    setAreaConfig(value)
+  }
 
   const fillterArea = () => {
-    console.log("douma ahihi");
-  };
+    console.log('douma ahihi')
+  }
 
   useEffect(() => {
     if (houseType !== defaultType || province !== defaultProvince) {
-      const type = get(houseType, "value", "bat-dong-san");
-      const location = get(province, "type", "toan-quoc");
-      console.log(district);
-      const pathname = Router.pathname || Router.route;
-      Router.push(`${pathname}/${type}/${location}`);
+      const type = get(houseType, 'value', 'bat-dong-san')
+      const location = get(province, 'type', 'toan-quoc')
+      console.log(district)
+      const pathname = Router.pathname || Router.route
+      Router.push(`${pathname}/${type}/${location}`)
     }
-  }, [houseType, province]);
+  }, [houseType, province])
 
   return (
     <div className="search">
@@ -182,15 +182,15 @@ const Search = props => {
       </Row>
       <style jsx>{styles}</style>
     </div>
-  );
-};
+  )
+}
 
 Search.propTypes = {
   type: string,
   location: string,
-};
+}
 
-export default Search;
+export default Search
 
 const styles = css`
   .search {
@@ -200,4 +200,4 @@ const styles = css`
   .filed {
     margin: 5px 10px;
   }
-`;
+`
